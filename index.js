aa395c1d1e0f2c1f4b8fd3c0e8ed72a633229a02
Harpreet Singh <hurpreet@icloud.com>
2023-05-06 04:34:16 -0400

    Lab Completed

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
 /* Your Code Here */
+function createEmployeeRecord(arr){
+    return{
+        firstName : arr[0],
+        familyName :arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents : [],
+        timeOutEvents : []
+}
+}
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+function createEmployeeRecords(arr){
+    const result= arr.map(record => {
+        return createEmployeeRecord(record)
+    })
+    return result
+}
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+function createTimeInEvent(date){
+    this.timeInEvents.push(createTimeEvent(date, "TimeIn"))
+    return this
+}
+
+function createTimeOutEvent(date){
+    this.timeOutEvents.push(createTimeEvent(date, "TimeOut"))
+    return this
+}
+//Helpers
+
+function createTimeEvent(date, type){
+    return{
+        type,
+        hour: parseInt(date.split(" ")[1]),
+        date: date.split(" ")[0]
+    }
+}
+
+function hoursWorkedOnDate(date){
+    const timeIn = this.timeInEvents.find(record => record.date === date)
+    const timeOut = this.timeOutEvents.find(record => record.date === date)
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+function wagesEarnedOnDate(date){
+    const numHours = hoursWorkedOnDate.call(this, date)
+    return numHours * this.payPerHour
+}
+
+function findEmployeeByFirstName(records, firstName){
+    return records.find(record => record.firstName === firstName)
+}
+
+function calculatePayroll(records){
+    return records.reduce((acc, record) => {
+        return allWagesFor.call(record)+ acc
+    }, 0)
+}
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -20,4 +67,6 @@ const allWagesFor = function () {
 
     return payable
 }
+
+
 
